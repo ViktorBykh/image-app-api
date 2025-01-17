cd0a3aa9d118052420b95c7e1d042487cdab6cdc
ViktorBykh <129959009+ViktorBykh@users.noreply.github.com>
2025-01-17 15:39:12 +0200

    fix: editing `AwsConfig`

--- src/main/java/image_app/api/config/AwsConfig.java
+++ src/main/java/image_app/api/config/AwsConfig.java
@@ -1,8 +1,10 @@
 package image_app.api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.rekognition.RekognitionClient;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -10,19 +12,28 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class AwsConfig {
 
+    @Value("${aws.accessKey}")
+    private String accessKey;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
+    @Value("${aws.region}")
+    private String region;
+
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
-                .region(Region.EU_CENTRAL_1)
-                .credentialsProvider(ProfileCredentialsProvider.create())
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
                 .build();
     }
 
     @Bean
     public RekognitionClient rekognitionClient() {
         return RekognitionClient.builder()
-                .region(Region.EU_CENTRAL_1)
-                .credentialsProvider(ProfileCredentialsProvider.create())
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
                 .build();
     }
 }
